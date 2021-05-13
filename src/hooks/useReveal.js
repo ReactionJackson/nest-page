5855b3b050d028b1f45ea8f98bca34907adad86b
Joseph Jackson <hello@josephmjackson.com>
2021-05-13 10:32:00 +0100

    fix content not showing on short screens

--- src/hooks/useReveal.js
+++ src/hooks/useReveal.js
@@ -3,7 +3,10 @@ import { useInView } from 'react-intersection-observer'
 
 export const useReveal = () => {
   const [ isRevealed, setIsRevealed ] = useState(false)
-  const { ref, inView } = useInView({ rootMargin: '-200px', triggerOnce: true })
+  const { ref, inView } = useInView({
+    rootMargin: window.innerHeight >= 600 ? '-200px' : '0px',
+    triggerOnce: true
+  })
   useEffect(() => {
     setIsRevealed(inView)
   }, [ inView ])
