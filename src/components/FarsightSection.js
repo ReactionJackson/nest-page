ad23ba9a96f2a27975b03c40233715df04a93493
Joseph Jackson <hello@josephmjackson.com>
2021-05-13 09:54:00 +0100

    reduce delay to measure page

--- src/components/FarsightSection.js
+++ src/components/FarsightSection.js
@@ -37,7 +37,7 @@ const FarsightSection = () => {
   }
 
   useEffect(() => {
-    setTimeout(() => updateInterpolationValue(), [ 1000 ])
+    setTimeout(() => updateInterpolationValue(), [ 200 ])
   }, [])
 
   useListener('resize', updateInterpolationValue)
